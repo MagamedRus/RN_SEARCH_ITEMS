7b73dd8cfd3776929d6462382c2cfb30eeba2b51
Muhammad <muhammadkasymov@yandex.ru>
2022-08-19 11:54:28 +0500

    fix width of text header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,5 +1,5 @@
 import React from "react"
-import { StyleSheet, Text, View, StatusBar } from "react-native"
+import { StyleSheet, Text, View, StatusBar, Dimensions } from "react-native"
 import ArrowButton from "./ArrowButton";
 
 
@@ -30,7 +30,7 @@ const styles = StyleSheet.create({
     textAlign: "left",
     textAlignVertical: 'center',
     fontSize: 19,
-    width: 210,
+    maxWidth: Dimensions.get('screen').width - 70,
     marginLeft: 15,
     marginTop: 5,
     fontFamily: "Inter-Medium",
